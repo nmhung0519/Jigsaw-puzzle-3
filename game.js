af330f98d0b72e2cba198c52595864e1c4ab63ba
Mạnh Hung <54991783+nmhung0519@users.noreply.github.com>
2020-07-01 20:36:01 +0700

    #14 Đổi nút next LV thành nút Home

--- game.js
+++ game.js
@@ -254,8 +254,10 @@ addEventListener("mouseup", function () {
 				for (var i = 0; i < n; i++) if (pieces[i][0].children[0].getAttribute('x') != i) rightRow = false;
 			}
 			if (rightRow && rightCol) {
-				win();
 				soundComplete.play();
+				if (index + 1 == game.length) nextText.innerText = "Home";
+				win();
+
 			}
 			else if (count == 0) gameOver();
 			else soundMove.play();
@@ -525,15 +527,28 @@ function win() {
 nextButton.onclick = function () {
 	if (!mute) soundClick.play();
 	canClick = 0;
-	console.log(index + 1, game.length);
-	if (index +1 < game.length) nextLV();
+	if (index + 1 < game.length) nextLV();
+	else {
+		var container_pre = document.getElementsByClassName("container");
+		container_pre = container_pre[container_pre.length - 2];
+		var header_pre = document.getElementsByClassName("header");
+		header_pre = header_pre[header_pre.length - 2];
+		menu.removeChild(container_pre);
+		menu.removeChild(header_pre);
+		setTimeout(function () {
+			container_pre.style.display = 'block';
+			header_pre.style.display = 'block';
+			nextText.innerText = "Next Level";
+		}, 800);
+		congrat.style.top = '-60px';
+		footer.style.bottom = '-60px';
+		_back();
+    }
 }
 function newGame() {
 	var data = game[index];
 	count = data['moves'];
-	console.log(play_title, count);
 	play_title.innerText = "Moves:" + count;
-	console.log(play_title.innerText);
 	rightCol = false;
 	rightRow = false;
 	footer.style.bottom = '-60px';
@@ -604,6 +619,5 @@ function createButton(i, data) {
 	but.className = 'button';
 	but.innerText = data['name'];
 	but.style.background = data['butBackground'];
-	console.log(but);
 	return but;
 }
